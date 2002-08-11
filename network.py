d82c446ecd4bc42e50b848bbf9fb140bf223b5a5
average <average>
2002-08-11 08:22:47 +0000

    Major change for Node to derive from bag instead of WVertex.  Bag type will now hold arcs and capacity values.  Semantics change accordingly (arc capacities accumulate when .add()'ed).  Had to copy Vertex attributes (._graph, .id) and many of Vertex's methods to Node. Unlike Vertex.update(), Node.update(), now based on bag, only updates on dict types.  For adding list of arcs, use Node += [x].  Both update and iadd allow additional parameter for specified whether tails need to be added to the graph, defaults to true. In Node._push(), made use of faster dict.update(node.flow, node), since node is now bag and assured of having valid values. Added Node.last_tick attribute to keep track when node was last activated (esp. relevant since old flow values are allowed to stay around). Fixed Node.clear() and Network.clear() so that they clear energy values as well. Network.add() no longer has collision parameter, since it is implied (i.e. ADD) with Node(bag) class. Created Network.io imbag attribute which will eventuall serve as a way to specify energy into/out of the network. Reversed previous "optimization" which caused bug in Network().  Said method was calling Network.energy.update(node.flow) within the same loop that was using the values of Network.energy.  XXX Was not able to come up with a decent doctest to catch this error. Fixed Network.update() to that if Nodes are merged, energy values also get added.  Created collision function MERGE_NODE for same.

--- network.py
+++ network.py
@@ -3,11 +3,13 @@
 
 """Network class for flow networks."""
 
-__version__ = "$Revision: 2.6 $"
+__version__ = "$Revision: 2.7 $"
 __author__  = "$Author: average $"
-__date__    = "$Date: 2002/08/09 21:58:51 $"
+__date__    = "$Date: 2002/08/11 08:22:47 $"
 
 #Add Network.time to track how many ticks
+
+from __future__ import generators
 
 import random
 from graph import *
@@ -15,16 +17,19 @@ from bag import *
 
 DEFAULT_CAPACITY = 1
 
-class Node(WVertex):
+class Node(bag, VMixin):
     """Node in a flow network."""
     
-    __slots__ = ['flow']
+    __slots__ = ['flow', 'last_tick', '_graph', '_id']
     
-    def __init__(self, network, id, init={}, capacity=DEFAULT_CAPACITY, collision=ADD):
-        super(Node, self).__init__(network, id, init, capacity, collision)
+    def __init__(self, network, id, init={}):
+        super(Node, self).__init__(init)
+        self._graph = network
+        self._id = id
         self.flow = imbag()
+        if init: network.add(list(init))    #XXX may add sinks to network multiple times
 
-    def add(self, sink, capacity=DEFAULT_CAPACITY, collision=ADD):
+    def add(self, sink, capacity=DEFAULT_CAPACITY):
         """Add connection to sink with given capacity (defaults to 1).
         >>> n = Network()
         >>> n[1].add([2, 3], 5)
@@ -37,25 +42,84 @@ class Node(WVertex):
         >>> print n[1]
         0 {2: 8, 3: 5}
         """
-        super(Node, self).add(sink, capacity, collision)
+        try:
+            self[sink] += capacity
+        except TypeError:
+            if not isinstance(sink, list): raise TypeError("argument must be hashable type or a list object.")
+            self += (sink*capacity)     #XXX slow if capacity large
     
-    def update(self, sinks, capacity=DEFAULT_CAPACITY, collision=ADD):
+    def update(self, sinks, add_sinks=1):
         """Update arcs leaving node.  If arc already exists, capacity is
         add to existing value; if not, it is created with default capacity.
         >>> n = Network()
-        >>> n[1].update([1, 1, 2, 3, 1])
+        >>> n[1] += [1, 1, 2, 3, 1]
         >>> print n[1]
         0 {1: 3, 2: 1, 3: 1}
         >>> n[1].update({2: 3, 4: 2})
         >>> print n[1]
         0 {1: 3, 2: 4, 3: 1, 4: 2}
-        >>> n[1].update([5, 1], 3)
+        >>> n[1] += ([5, 1] * 3)
         >>> print n[1]
         0 {1: 6, 2: 4, 3: 1, 4: 2, 5: 3}
+        >>> assert 1 in n and 2 in n and 3 in n and 4 in n and 5 in n
         """
-        super(Node, self).update(sinks, capacity, collision)
+        super(Node, self).update(sinks)
+        if add_sinks:
+            g = self._graph
+            for s in sinks:
+                if s not in g:
+                    g.add(s)
+                    
+    def __iadd__(self, sinks, add_sinks=1):
+        
+        if add_sinks:
+            g = self._graph
+            for s in sinks:
+                if s not in g:
+                    g.add(s)
+        return super(Node, self).__iadd__(sinks)
+
+    def in_vertices(self):  #O(n)
+        """Return iterator over the nodes that point to self.
+        >>> n = Network()
+        >>> n.add(1, [2, 3, 4])
+        >>> n.add(2, [3, 2])
+        >>> list(n[2].in_vertices())       #XXX arbitrary order
+        [1, 2]
+        """
+        for head in self._graph.itervalues():
+            if self._id in head:
+                yield head._id
+
+    out_vertices = bag.iterkeys
     
-    def _push(self, bits):
+    def in_degree(self):
+        """Return number of edges pointing into vertex.
+        >>> n = Network()
+        >>> n.add(1, [2, 3, 4])
+        >>> n.add(2, [3, 2])
+        >>> n[1].in_degree(), n[2].in_degree(), n[4].in_degree()
+        (0, 2, 1)
+        """
+        return len(list(self.in_vertices()))
+    
+    out_degree = dict.__len__
+
+    def __setitem__(self, sink, capacity):
+        """Set arc capacity.  If sink does not exist and capacity>0, 
+        sink is created and added to network.
+        >>> n = Network()
+        >>> n[1][3]
+        0
+        >>> n[1][2] = 3
+        >>> print n
+        {1: 0 {2: 3}, 2: 0 {}}
+        """
+        super(Node, self).__setitem__(sink, capacity)
+        if sink not in self._graph:
+            self._graph.add(sink)
+        
+    def _push(self, bits, tick):
         """Advance node 1 time increment. Returns amount of energy remaining.
         Should not be run except through Network().
         >>> n = Network()
@@ -73,6 +137,7 @@ class Node(WVertex):
         {1: -2 {2: 2}, 2: -1 {}}
         """
         assert bits #should not be called with 0 bits
+        self.last_tick = tick
         self.flow.clear()   #clear old flow values
         if bits >= 0:
             paths = reduce(operator.add, [[tail]*capacity for tail, capacity in self.iteritems()], [])
@@ -84,7 +149,7 @@ class Node(WVertex):
             bit = -1
             assert len(paths) == self.sum_in()
         if bits>=len(paths):
-            self.flow.update(self)  #all arcs saturated
+            dict.update(self.flow, self) #all arcs saturated; can do dict.update since we know self.flow=={}
             return bits - len(paths)
         while bits and paths:
             path = paths.pop(int(random.random()*len(paths))) 
@@ -104,6 +169,10 @@ class Node(WVertex):
     energy_out = property(_energy_out, None, None, "Energy out of node.")
     energy = property(_energy_read, _energy_write, None, "Energy at node. Slow -- use network.energy[id]")
 
+    def clear(self):
+        super(Node, self).clear()
+        self.flow.clear()
+
     def __str__(self):
         """Returns string with energy level and arc info.
         >>> n = Network()
@@ -112,68 +181,116 @@ class Node(WVertex):
         >>> print n[1]
         5 {2: 1}
         """
-        return str(self.energy) + " " + super(Node, self).__str__("%r: %s")
-        
+        return "%r %r" % (self.energy, self)
+
     def validate(self):
-        #assert self.energy >= 0, "Invalid energy value %r at node %r" % (self.energy, self._id)
+        """Assert Node invariants."""
+        hash(self._id) #id should be hashable
+        assert isinstance(self._graph, Network)
+        assert self._id in self._graph
         for node, cap in self.iteritems():
-            assert cap >= 0, "Invalid capacity at node %r: %r" % (node, cap)
-        super(Node, self).validate()
+            assert node in self._graph, "Non-existant tail %r in node %r" % (node, self._id)
+            assert cap > 0, "Invalid capacity at node %r: %r" % (node, cap)
         
+def MERGE_NODE(net, node, sinks): 
+    net[node].update(sinks)
+    try: net[node].energy += sinks.energy
+    except AttributeError: pass
 
 class Network(Graph):
     """Flow network class."""
 
-    __slots__ = ['energy', 'ticks']
+    __slots__ = ['energy', 'io', 'ticks']
 
     def __init__(self, init={}, VertexType=Node):
+        if not isinstance(VertexType, type(Node)): raise TypeError, "Invalid vertex type"
         super(Network, self).__init__(init, VertexType)
-        self.energy = imbag()
-        self.ticks = 0
+        self.energy = imbag()   #stores energy values at each node
+        self.io = imbag()       #energy entering and leaving the network
+        self.ticks = 0          #number of network clock ticks since creation
 
-    def add(self, head, tail=[], capacity=DEFAULT_CAPACITY, edge_collision=ADD):
+    def add(self, source, sink=[], capacity=DEFAULT_CAPACITY):
         """Like Graph.add, but will accumulate edge values, instead of
         overwriting them.  See Graph.add.__doc__.
         >>> n = Network()
         >>> n.add(1, [2, 3], 2)
         >>> n.add(1, 2)
+        >>> n.add(4, 1)
         >>> print n
-        {1: 0 {2: 3, 3: 2}, 2: 0 {}, 3: 0 {}}
+        {1: 0 {2: 3, 3: 2}, 2: 0 {}, 3: 0 {}, 4: 0 {1: 1}}
         """
-        super(Network, self).add(head, tail, capacity, edge_collision)
-                    
+        if not isinstance(sink, list): sink = [sink]
+        try:  #single node addition
+            self[source] += (sink * capacity)   #XXX inefficient if capacity large
+        except TypeError:  #multiple node addition
+            if not isinstance(source, list): raise "source must be hashable object or list type"
+            if sink: self.add(sink)
+            for s in source:  #XXX will add same tails multiple times
+                self[s].__iadd__(sink*capacity, 0)
+
+    def update(self, other, default=None, collision=MERGE_NODE):
+        """Merges one graph with another.  All vertices will be convertex to VertexType.  Takes union of edge
+        >>> n, g = Network(), Graph()
+        >>> n.add(1, [1, 2])
+        >>> g.add(3, [2, 3]); g.add(1, 2, 3); g.add(1, 4, 2)
+        >>> n.update(g)
+        >>> print n
+        {1: 0 {1: 1, 2: 4, 4: 2}, 2: 0 {}, 3: 0 {2: 1, 3: 1}, 4: 0 {}}
+        >>> g.add(3, 5)  #changes to g should not affect n
+        >>> n.validate()
+        >>> n2 = Network(g)
+        >>> n2.energy.update({1: 5, 3: 1})
+        >>> print n2
+        {1: 5 {2: 3, 4: 2}, 2: 0 {}, 3: 1 {2: 1, 3: 1, 5: 1}, 4: 0 {}, 5: 0 {}}
+        >>> n.energy[1] += 2
+        >>> n.update(n2)
+        >>> print n
+        {1: 7 {1: 1, 2: 7, 4: 4}, 2: 0 {}, 3: 1 {2: 2, 3: 2, 5: 1}, 4: 0 {}, 5: 0 {}}
+        
+        """
+        assert isinstance(other, Graph), "Can only merge Graph types."
+        for nid, node in other.iteritems():
+            if nid in self:  #do union of edge sets
+                collision(self, nid, node)
+            else:   #otherwise need to copy the set
+                self[nid] = self.VertexType(self, nid, node)
+                try:  self.energy[nid] += node.energy
+                except AttributeError: pass
+
     def __call__(self, ticks=1):
         """Advance network for specified ticks, defaults to 1.
         >>> n = Network()
-        >>> n.add(1, 2, 2)
-        >>> n.add(2, 3)
+        >>> n.add(1, [2, 4], 2)
+        >>> n.add([2, 4], 3)
         >>> n.add(3, 1)
         >>> n.energy[1] += 4
         >>> n()
-        >>> assert n.energy == {1: 2, 2: 2}
+        >>> assert n.energy == {2: 2, 4: 2}
         >>> n()
-        >>> assert n.energy == {2: 3, 3: 1}
-        >>> n.energy[1] += 4
+        >>> assert n.energy == {2: 1, 3: 2, 4: 1}
+        >>> n.energy[1] += 9
         >>> n(2)
-        >>> assert n.energy == {1: 2, 2: 5, 3: 1}
+        >>> assert n.energy == {1: 3, 2: 3, 3: 4, 4: 3}
         >>> n.ticks
         4
 
         Also aborts once flow reaches zero.
         >>> n.discard(3, 1)
         >>> n(100)
-        >>> assert n.energy == {3: 8}
+        >>> assert n.energy == {3: 13}
         >>> n.ticks
-        11
+        9
         """
         assert ticks>=0     #may desire ticks<0 to run in reverse
         energy = self.energy
         for tic in xrange(ticks):
             flow = 0
+            energy.update(self.io)
             active_nodes = map(operator.getitem, [self]*len(energy.keys()), energy.iterkeys())
             for node in active_nodes:
-                energy[node._id] = node._push(energy[node._id])
+                energy[node._id] = node._push(energy[node._id], self.ticks)
                 flow |= (node.flow != {})
+            for node in active_nodes:   #have to wait until all flow calculations done to avoid adding energy to unvisited nodes.
                 energy.update(node.flow)
             if flow: self.ticks += 1
             else: break
@@ -209,6 +326,11 @@ class Network(Graph):
         return sum
 
     flow = property(_flow, None, None, "Total energy moved on last tick.")
+
+    def clear(self):
+        super(Network, self).clear()
+        self.energy.clear()
+        self.ticks = 0
         
     def validate(self):
         for node in self.itervalues():

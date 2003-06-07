af031693c660a27301fea6724222ef5cfe9c2b11
average <average>
2003-06-07 22:16:35 +0000

    Re-added "from __future__" line to be able to test with Python v2.2. Added sum() if not already defined (again for Python v2.2). Fixed bug and cleaned-up imbag.__init__().  If bag.__init__(imbag) was called, no validation was occuring. Cleared-up imbag.update() at cost of efficiency.  Still need to fix what happens when invalid values are encountered mid-stream. Added new imbag.fromkeys() class method to override new dict.fromkeys.  New method will accumulate counts instead of overwrite.  Can now simplify imbag.__init__()! (TODO) imbag.pop() now raises NotImplementedError instead of wrong NotImplemented. Added new imbag._test_invariant() staticmethod to make it simpler to define subtypes without having to override a lot of functions. imbag.__imul__, setdefault, fromkeys, __setitem__, _validate now call _test_invariant staticmethod.  Bag can now use this directly instead of overriding each. Removed imbag.__eq__(), dict's version should be sufficient and less inappropriately flexible.  If user wants to compare bag to list, just apply bag.fromkeys(list) first (__eq__ was doing this anyway). Removed imbag.__len__() -- it didn't make much sense for negative values.  Moved to bag where it does make (some) sense. Removed bag.__imul__, setdefault, __setitem__, _validate methods.  Baseclass methods will be sufficient now with new _test_invariant().  Docstrings conflated appropriately. Added some miscellaneous docstring tests under _test() which no longer really fit under any particular existing method.

--- bag.py
+++ bag.py
@@ -3,17 +3,23 @@
 
 """Bag types"""
 
-__version__ = "$Revision: 2.5 $"
+__version__ = "$Revision: 2.6 $"
 __author__  = "$Author: average $"
-__date__    = "$Date: 2003/06/05 02:16:39 $"
-
-import operator
+__date__    = "$Date: 2003/06/07 22:16:35 $"
 
 #bag should have list interface? --should add list methods: append, remove, min/max,  __str__ return list string, etc.
 #   in addition to min/max(), perhaps create most/least() to return the item with the highest/lowest count
 #XXX figure out what should happen: bag[nokey] -= 5, bag[key] -=5
 #XXX checking in setitem takes too much time: create compress function that removes zero-valued items periodically
 #create bag attribute to hold size of bag and calculate only when bag has been written to.
+#perhaps limit bag to either list or dict types for adding/updating.
+
+from __future__ import generators
+
+if 'sum' not in dir(__builtins__):
+    import operator
+    def sum(iterable):
+        return reduce(operator.add, iterable, 0)
 
 IntegerType = (int, long)
 
@@ -27,55 +33,81 @@ class imbag(dict):
         >>> b = bag()   #creates empty bag
         >>> b
         {}
-        >>> b = bag({'b': 2, 'c': 1, 'a': 3})
-        >>> b2 = bag(['a', 'b', 'c', 'b', 'a', 'a'])
-        >>> b3 = bag("abacab")
-        >>> assert b == b2 == b3
-        >>> b = imbag({1: -1, 2: 0, 3: -9})
-        >>> assert b == {1: -1, 3: -9}
-        >>> word_count = bag("how much wood could a wood chuck chuck".split())
-        >>> assert word_count == {'how': 1, 'much': 1, 'wood': 2, 'could': 1, 'a': 1, 'chuck': 2}
+        >>> ib = imbag({1: -1, 2: 0, 3: -9})
+        >>> assert ib == {1: -1, 3: -9}
         """
-        if isinstance(init, dict) or init==[]:
-            dict.__init__(self, init)
-            if init!={} and not isinstance(init, imbag):   #non-bag may have questionble count values
-                self._validate()
-        else:   #assume sequence initializer
+        #XXX use new fromkeys method for sequence inits!
+        if init=={} or isinstance(init, self.__class__) or init==[]:
+            dict.__init__(self, init)   #values known to be good, use faster dict creation
+        else:
             dict.__init__(self)
-            self += init
+            if isinstance(init, dict): self.update(init)
+            else: self += init
 
     def update(self, items):
         """Adds contents to bag.  Takes dictionary type.
-        >>> b = imbag("abc")
-        >>> b.update({'a': 2, 'b': 1, 'c': 0})
-        >>> assert b == {'a': 3, 'b': 2, 'c': 1}
+        >>> ib = imbag('abc')
+        >>> ib.update({'a': 2, 'b': 1, 'c': 0})
+        >>> assert ib == {'a': 3, 'b': 2, 'c': 1}
 
         Negative updates are allowable.
-        >>> b.update({'a': -2, 'b': -2, 'c': -2, 'd': 2})
-        >>> assert b == {'a': 1, 'c': -1, 'd': 2}
+        >>> ib.update({'a': -2, 'b': -2, 'c': -2, 'd': 2})
+        >>> assert ib == {'a': 1, 'c': -1, 'd': 2}
 
-        Invalid values raise TypeError and may leave bag in
-        unknown state:
-        >>> b.update({0: 'test1', 'a': 'test2', 'd': 3, 'f': 1.0})
+        Invalid values are skipped, and will raise TypeError.
+        >>> ib.update({0: 'test1', 'a': 'test2', 'd': 3, 'f': 1.0, 'c': 2.0})
         Traceback (most recent call last):
         ...
-        TypeError: unsupported operand type(s) for +: 'int' and 'str'
+        TypeError: must use integral type for bag values, not <type 'float'>
+        >>> assert ib == {'a': 1, 'c': -1, 'd': 5}
+
+        Trying to update bag with negative values raises ValueError:
+        >>> b = bag()
+        >>> b.update(ib)
+        Traceback (most recent call last):
+        ...
+        ValueError: bag values must be non-negative: -1
         """
-        #XXX may be able to improve this using map and operator functions
-        #XXX what if dict values are floats?
+        #XXX may be able to improve this by calling dict methods directly and/or using map and operator functions
+        #we don't want to abort mid-update, so validate at end of loop
+        #XXX should raise exception and return unchanged self if problem encountered!
+        bad_value_encountered = False
         for key, count in items.iteritems():
-            if key in self:
-                newvalue = dict.__getitem__(self, key) + count
-                if newvalue: dict.__setitem__(self, key, newvalue)
-                else: del self[key]
+            try:
+                self[key] += count  #may be slower than necessary
+            except (TypeError, ValueError):
+                bad_value_encountered, bad_value = True, count
+        if bad_value_encountered: self._test_invariant(bad_value)
+        #if not isinstance(items, imbag): self._validate()
+
+    def fromkeys(cls, iterable, count=1):
+        """Class method which creates bag from iterable adding optional count for each item.
+
+        >>> b = bag({'b': 2, 'c': 1, 'a': 3})
+        >>> b2 = bag.fromkeys(['a', 'b', 'c', 'b', 'a', 'a'])
+        >>> b3 = bag.fromkeys("abacab")
+        >>> assert b == b2 == b3
+
+        >>> word_count = bag.fromkeys("how much wood could a wood chuck chuck".split())
+        >>> assert word_count == {'how': 1, 'much': 1, 'wood': 2, 'could': 1, 'a': 1, 'chuck': 2}
+
+        An optional count can be specified.  Count added each time item is encountered.
+        >>> assert bag.fromkeys("abacab", 5) == {'a': 15, 'b': 10, 'c': 5}
+        """
+        b = cls()
+        b._test_invariant(count)
+        for key in iterable:
+            if key in b:
+                dict.__setitem__(b, key, dict.__getitem__(b, key) + count)
             else:
-                dict.__setitem__(self, key, count)
-            #self[key] += count  #about twice as slow than above
-        if not isinstance(items, imbag): self._validate()
+                dict.__setitem__(b, key, count)
+        return b
+
+    fromkeys = classmethod(fromkeys)
 
     def pop(self):
         """Removes and returns one item from the bag."""
-        raise NotImplemented #XXX
+        raise NotImplementedError #XXX
 
     def discard(self, item, count=None):
         """Removes count number of the specified item.  If count is not
@@ -92,7 +124,13 @@ class imbag(dict):
         else:
             self[item] -= count
 
-    def __iadd__(self, items):
+    def setdefault(self, key, count=1):
+        #XXX perhaps remove this on bag
+        self._test_invariant(count)
+        if count:  return dict.setdefault(self, key, count)
+        else:  return self[key]
+
+    def __iadd__(self, iterable):
         """Add items in iterable object to bag.
         >>> b = bag()
         >>> b += [1, 2, 1, 0]
@@ -101,7 +139,7 @@ class imbag(dict):
         >>> b += "abca"
         >>> assert b == {'a': 2, 'b': 1, 'c': 1}
         """
-        for key in items:
+        for key in iterable:
             if key in self:
                 newvalue = dict.__getitem__(self, key) + 1
                 if newvalue: dict.__setitem__(self, key, newvalue)
@@ -110,77 +148,47 @@ class imbag(dict):
                 dict.__setitem__(self, key, 1)
         return self
 
-    def __isub__(self, items):
+    def __isub__(self, iterable):
         """Subtract items in iterable object from bag.
         >>> b = bag("abacab")
         >>> b -= "cab"
         >>> assert b == {'a': 2, 'b': 1}
         """
         #XXX Should this method be removed? (list doesn't support it)  What should happen if item not in bag?
-        for item in items:
+        for item in iterable:
             self[item] -= 1
         return self
 
     def __imul__(self, factor):
         """Multiply bag contents by factor.
-        >>> b = imbag("abacab")
-        >>> b*= 4
+        >>> b = bag("abacab")
+        >>> b *= 4
         >>> assert b == {'a': 12, 'b': 8, 'c': 4}
-        >>> b*= -1
-        >>> assert b == {'a': -12, 'b': -8, 'c': -4}
-        >>> b*=0
-        >>> assert b == {}
+
+        Negative factors can be used with imbag.
+        >>> ib = imbag(b)
+        >>> ib *= -1
+        >>> assert ib == {'a': -12, 'b': -8, 'c': -4}
+
+        Trying that on a standard bag will raise ValueError.
+        >>> b *= -1
+        Traceback (most recent call last):
+        ...
+        ValueError: bag values must be non-negative: -1
+
+        Zero factors will return empty bag.
+        >>> b *= 0
+        >>> b
+        {}
+
         """
-        if not isinstance(factor, IntegerType):
-            raise TypeError, "unsupported operand type(s) for *: %r and %r" % (type(self), type(other))
+        self._test_invariant(factor)
         if factor:
             for item, count in self.iteritems():
                 dict.__setitem__(self, item, count*factor) #bypass test logic in bag.__setitem__
         else:   #factor==0
             self.clear()
         return self
-
-    def __eq__(self, other):
-        """Equality test.  Can compare against dict or sequence type.
-        >>> bag("abacab") == {'a': 3, 'b': 2, 'c': 1}
-        True
-        >>> bag([1, 2, 1, 3, 1]) == [1, 1, 1, 3, 2]
-        True
-        """
-        if isinstance(other, list): other = self.__class__(other)
-        return dict.__eq__(self, other)
-
-    def setdefault(self, key, count=1):
-        #XXX perhaps remove this on bag
-        if not isinstance(count, IntegerType):  raise TypeError, "value must be integral type: %r" % count
-        elif count:  return dict.setdefault(self, key, count)
-        else:  return self[key]
-
-    def __nonzero__(self):
-        """Returns zero if bag is empty, non-zero otherwise.
-        >>> b = bag()
-        >>> not b
-        True
-        >>> b += [0]
-        >>> not b
-        False
-        """
-        return dict.__len__(self)
-
-    def __len__(self):
-        """Returns total number of items in bag.
-        >>> b = bag("abacab")
-        >>> len(b)
-        6
-
-        Returns sum of item counts for imbag.
-        >>> b = imbag(b)
-        >>> b['a'] = -4
-        >>> len(b)
-        -1
-        """
-        #XXX len(b)!=len(list(b)) -- perhaps should return abs (or iter not return negatived keys)
-        return sum(self.itervalues())
 
     def __abs__(self):
         """Returns sum of absolute value of item counts in bag.
@@ -208,33 +216,40 @@ class imbag(dict):
     count = __getitem__
 
     def __setitem__(self, item, count):
-        """Sets the count for the given item.
-        >>> b = imbag([1, 2, 3])
+        """Sets the count for the given item in bag.
+        >>> b = bag([1, 2, 3])
         >>> b[1] = 3
         >>> b[4] = 2L   #long values okay
         >>> assert b == {1: 3, 2: 1, 3: 1, 4: 2}
 
-        If count is zero, all items are deleted from bag.
+        If count is zero, all 'matching items' are deleted from bag.
         >>> b[2] = 0
         >>> assert b == {1: 3, 3: 1, 4: 2}
 
         Counts for imbag are allowed to be negative.
+        >>> ib = imbag(b)
+        >>> ib[4] = -2
+        >>> ib[5] -= 2
+        >>> ib[1] -= 4
+        >>> ib[3] -= 1
+        >>> assert ib == {1: -1, 4: -2, 5: -2}
+
+        Setting negative values on standard bag raises ValueError.
         >>> b[4] = -2
-        >>> b[5] -= 2
-        >>> b[1] -= 4
-        >>> b[3] -= 1
-        >>> assert b == {1: -1, 4: -2, 5: -2}
+        Traceback (most recent call last):
+        ...
+        ValueError: bag values must be non-negative: -2
 
         If count is non-integer, TypeError is raised.
         >>> b[1] = "oops"
         Traceback (most recent call last):
         ...
-        TypeError: count must be integral type, not <type 'str'>
+        TypeError: must use integral type for bag values, not <type 'str'>
         """
-        if not isinstance(count, IntegerType): raise TypeError, "count must be integral type, not %r" % type(count)
-        elif count:
+        self._test_invariant(count)
+        if count:
             dict.__setitem__(self, item, count)
-        else:
+        else:   #setting to 0 so delete key
             try:  del self[item]
             except LookupError:  pass
 
@@ -270,90 +285,61 @@ class imbag(dict):
         l.sort()
         return str(l)
 
+    def _test_invariant(value):
+        """Raises TypeError if value is not integral type."""
+        if not isinstance(value, IntegerType):
+            raise TypeError, "must use integral type for bag values, not %r" % type(value)
+
+    _test_invariant = staticmethod(_test_invariant)
+
     def _validate(self):
-        remove_list = []
-        for key, count in self.iteritems():
-            if not isinstance(count, IntegerType):
-                raise TypeError, "invalid count type %r" % count
-            elif count==0:
-                remove_list.append(key) #can't delete while iterating
-        for key in remove_list:
-            del self[key]
+        for count in self.itervalues():
+            self._test_invariant(count)
+            assert count
 
 
 class bag(imbag):
-    """Standard bag class."""
+    """Standard bag class.  Allows only non-negative bag counts."""
 
     __slots__ = []
 
-    def __imul__(self, factor):
-        """Multiply bag contents by factor.
+    def __len__(self):
+        """Returns total number of items in bag.
         >>> b = bag("abacab")
-        >>> b *= 4
-        >>> assert b == {'a': 12, 'b': 8, 'c': 4}
-
-        Negative or zero factors will return empty bag.
-        >>> b *= -2
-        >>> b
-        {}
-        >>> b += ['a', 'b', 'a']
-        >>> b *= 0
-        >>> b
-        {}
+        >>> len(b)
+        6
         """
-        if not isinstance(factor, IntegerType):
-            raise TypeError, "unsupported operand type(s) for *: %r and %r" % (type(self), type(other))
-        if factor > 1:
-            for item, count in self.iteritems():
-                dict.__setitem__(self, item, count*factor) #bypass test logic in bag.__setitem__
-        elif factor <= 0:
-            self.clear()    #duplicates list.__imul__ behavior
-        return self
+        return sum(self.itervalues())
 
-    def setdefault(self, key, count=1):
-        #XXX perhaps remove this on bag
-        if not isinstance(count, IntegerType):  raise TypeError, "value must be integral type: %r" % count
-        elif count<0:  raise ValueError, "value must be >= 0: %r" % count
-        elif count==0:  return self[key]
-        else:  return dict.setdefault(self, key, count)
+    def _test_invariant(value):
+        """Tests baseclass invariants, plus raises ValueError if value is negative. """
+        imbag._test_invariant(value)
+        if value < 0:
+            raise ValueError, "bag values must be non-negative: %r" % value
 
-    def __setitem__(self, item, count=1):
-        """Like imbag.__setitem__, however if count is less than zero, ValueError is raised.
-        >>> b = bag([1, 2, 3, 4, 4])
-        >>> b[4] = -2
-        Traceback (most recent call last):
-        ...
-        ValueError: 4 depleted
-
-        #>>> b[5] = -2
-        #Traceback (most recent call last):
-        #...
-        #ValueError: 5 depleted
-        """
-        if not isinstance(count, IntegerType): raise TypeError, "count must be integral type, not %r" % type(count)
-        elif count>0:
-            dict.__setitem__(self, item, count)
-        else:   #XXX fix this, inconsistent: bag[key]=-1 vs bag[nokey]=-1
-            try:
-                del self[item]
-            except LookupError:
-                pass
-            else:
-                if count<0:
-                    raise ValueError, "%r depleted" % item
-
-    def _validate(self):
-        remove_list = []
-        for key, count in self.iteritems():
-            if not isinstance(count, IntegerType) or count<0:
-                raise ValueError, "invalid count value %r" % count
-            elif count==0:
-                remove_list.append(key) #can't delete while iterating
-        for key in remove_list:
-            del self[key]
+    _test_invariant = staticmethod(_test_invariant)
 
 
 def _test():
+    """Miscillaneous tests:
+
+    Equality test.  Can compare against dictionary or bag types.
+    >>> bag("abacab") == {'a': 3, 'b': 2, 'c': 1}
+    True
+    >>> b, l = bag([1, 2, 1, 3, 1]), [1, 1, 1, 3, 2]
+    >>> b == l
+    False
+    >>> b == bag(l)
+    True
+    >>> b = bag()
+
+    Tests for non-zero:
+    >>> bool(b)
+    False
+    >>> b += [0]
+    >>> bool(b)
+    True
+    """
     import doctest, bag
     return doctest.testmod(bag, isprivate=lambda i, j: 0)
 

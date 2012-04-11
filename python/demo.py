69ba228132cfc123eeebf76cbcf272f47bd703c5
Mark J <dreamingforward@gmail.com>
2012-04-11 00:55:16 -0500

    Added frame object to hold the objects as preliminary to new class

--- python/demo.py
+++ python/demo.py
@@ -33,13 +33,14 @@ from network import * #pangaia module
 ###Rule of Thumb #1:  Put the most arbitrary values at the top of the code.  Let users tweak.
 num_nodes = 100                   #number of nodes to draw
 p_nodes_lit = 0.07                #light x% of the nodes, chosen randomly.  Try 0, and then click to add a light source.
-want_labels = True                #turn off for only spheres
+want_labels = False                #turn off for only spheres
 
 ###Rule of Thumb #2:  Variable casing should be dynamic -- if variable is a part of a larger whole, use lower_case, otherwise CamelCase.
 UserNodeName = "Neophyte"  #This will be your personal Node name as seen from the view of the social garden
 default_name = "node"      #Nodes will take this name plus a number
 
 ###Visual-python constants
+scene.autoscale = True
 scene.title = UserNodeName or raw_input("Name:") #prompt for user's name if not registered, "think for the future: what name do you want to be addressed as in eternity?"
 scene.width, scene.height = 1440, 900  #XXX change this to fit your screen, pixels.
 scene.ambient = color.gray(0.05)   #sets background light:  should be a function of how many viewers rather than arbitrary
@@ -79,11 +80,11 @@ def rand3tuple(start = -world_radius, stop = world_radius):
 #This looks nice: inner sphere: chrome, light.color=green, outsphere=glass, opacity=0.4
 #Also, inner sphere: glass, color=gray, opacity=0.5, outsphere, glass, color=green, opacity=0.3
 #Note: materials = {wood, rough, marble, plastic, earth, diffuse, emissive, unshaded, shiny, chrome, blazed, silver, BlueMarbe, bricks}
-def node(name, pos, mass=mean_node_radius, valence=base_color, lit=False, noosphere=mean_node_radius/2, **kwargs):  #noosphere argument arbitrary
+def gnode(frame, name, pos, mass=mean_node_radius, valence=base_color, lit=False, noosphere=mean_node_radius/2, **kwargs):  #noosphere argument arbitrary
     """Draws a node on the screen.  Currently, a node is a sphere, plus an atmosphere."""
     n.add(name)
     n[name].energy = mass
-    if want_labels: label(pos=pos, text=name, box=False, opacity=0, color=color.gray(0.4), line=False, height=6)
+    if want_labels: label(frame=frame, pos=pos, text=name, box=False, opacity=0, color=color.gray(0.4), line=False, height=6)
     hsvcolor = vector(color.rgb_to_hsv(valence))
     if lit:  #primitve activity model:  make it a light source  #local_light(pos=pos, color=(1.0,0,0)) or at least maximize saturation #XXX is this working?
         local_light(pos=pos, color=color.white)
@@ -91,16 +92,13 @@ def node(name, pos, mass=mean_node_radius, valence=base_color, lit=False, noosph
     else:
         hsvcolor[2] *= 0.3 #reducing lightness
     valence = color.hsv_to_rgb(hsvcolor)
-    sphere(pos=pos, color=valence, opacity=base_opacity, radius=mass, material=materials.emissive if lit else scene.material, **kwargs) #inner sphere  (Note: try material=chrome)
-    sphere(pos=pos, color=atmos(valence), opacity=0.2*base_opacity, radius=mass+noosphere, material=materials.emissive, **kwargs) #outer sphere, color should be converted to hsv and value amplified assuming ambient color is shade of pure white
+    sphere(frame=frame, pos=pos, color=valence, opacity=base_opacity, radius=mass, material=materials.emissive if lit else scene.material, **kwargs) #inner sphere  (Note: try material=chrome)
+    sphere(frame=frame, pos=pos, color=atmos(valence), opacity=0.2*base_opacity, radius=mass+noosphere, material=materials.emissive, **kwargs) #outer sphere, color should be converted to hsv and value amplified assuming ambient color is shade of pure white
 
 ###################################################################
 
 n = Network()  #change this to use Network rather than Graph.
-
-### Rotation should be tried...
-##for i in arange(0,pi):
-##    f.axis=(0,i,0)
+f = frame()    #window frame that will hold all the objects
 
 ##Depth-cueing experiment #2: simply place semi-tranparent, scene.ambient-colored boxes from front to back.  They'll get progressively darker away from the user.
 #for i in range(10000): #-world_radius, +world_radius*2, int(world_radius/1)):
@@ -116,8 +114,15 @@ for i in range(num_nodes):
     if light:
         print "lit", name
         
-    node(name, rand3tuple(), random.randint(1,mean_node_radius), base_color, light, random.randint(int(mean_node_radius/2), mean_node_radius*2))
+    gnode(f, name, rand3tuple(), random.randint(1,mean_node_radius), base_color, light, random.randint(int(mean_node_radius/2), mean_node_radius*2))
 
+scene.autoscale=False
+
+##
+### Rotation should be tried...
+##for i in arange(1,360,0.1):
+##    rate(100)
+##    f.rotate(angle=pi/3600)
 
 ### Experiment in edge drawing
 ##nodes = scene.objects[:]
@@ -128,13 +133,13 @@ for i in range(num_nodes):
 
 ## Simple event handling, currently only can click nodes and affect radius
 while True:
-    rate(100)  #sets an upper-bound on how often (per second) the loop runs.
+    rate(100)  #sets an upper-bound on how often the loop runs.
     #f=scene.forward
     #print(scene.forward)
     if scene.mouse.events:
         mm=scene.mouse.getevent()
         if mm.pick==None: #create new sphere XXX just testing
-            node("light", pos=mm.pos, lit=True)
+            gnode(f, "light", pos=mm.pos, lit=True)
         else:
             print mm.pick, mm.pick.radius
             if mm.shift:  #XXX this is crude need to send this to node object so it both spheres get adjusted

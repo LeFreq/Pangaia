e493d58a9aada8ad41ac6e945becf473d7c37939
Mark J <dreamingforward@gmail.com>
2012-08-10 18:30:44 -0500

    better material for lights
    (It's prettier anyway….)

--- python/demo.py
+++ python/demo.py
@@ -13,7 +13,7 @@ Rotate scene with the right-mouse drag and Zoom using both buttons (Mac: use Opt
 #1) Create a node class derived from sphere object.
 #2) Add ability to Label a node.
 #3) Add vote-way up and vote way-down, animatng the nodes into and out of view along the z axis. (Navigator view)
-#4) Add ability to Link/associate nodes.  Animate linking with some [[gravity model]], function of node mass, etc.
+#4) Add ability to Link/associate nodes.  Animate linking with some [[gravity model]], function of node mass, etc.  Note, place somewhat randomly, and then check for collision (follow ethernet example).
 #5) Add ability to select a number of nodes and Group them.
 #6) Currently drawing random nodes.  Create an interface to import gmail messages and prompt user to sort.
 #7) since vpython has no depth shading, use built-in camera w/eye tracking to move the scene around as head moves.
@@ -27,6 +27,8 @@ Rotate scene with the right-mouse drag and Zoom using both buttons (Mac: use Opt
 #Also, using the built-in camera with eye-tracking for depth discernment (idea from Geoff Chesshire)
 #Notes: on #3, for determining animation, consider the number of links akin to inertial mass, use that (not "rest mass") to determine who/what should move.
 
+#XXX Remove user-spin to stop the addiction to eye-candy and implement gravity model
+
 from __future__ import division   #XXX: shouldn't have to do this -- isn't it the future already?
 
 from visual import *  #this pulls in the visual python library from VPython
@@ -36,7 +38,7 @@ from network import * #pangaia module
 ###Rule of Thumb #1:  Put the most arbitrary values at the top of the code.  Let users tweak.
 num_nodes = 100                   #number of nodes to draw
 p_nodes_lit = 0.05                #light x% of the nodes, chosen randomly.  Try 0, and clicking to add a light source: real-time 3-d light projection.
-want_labels = False                #turn off for only spheres
+want_labels = True                #turn off for only spheres
 
 ###Rule of Thumb #2:  Variable casing should be dynamic -- if variable is a part of a larger whole, use lower_case, otherwise CamelCase.
 UserNodeName = "Neophyte"  #This will be your personal Node name as seen from the view of the social garden
@@ -87,7 +89,7 @@ def gnode(frame, name, pos, mass=mean_node_radius, valence=base_color, lit=False
     """Draws a node on the screen.  Currently, a node is a sphere, plus an atmosphere."""
     n.add(name)
     n[name].energy = mass
-    if want_labels: label(frame=frame, pos=pos, text=name, box=False, opacity=0, color=color.gray(0.4), line=False, height=6)
+    if want_labels: label(frame=frame, pos=pos, text=name, box=False, opacity=0, color=color.gray(0.4), line=False, height=8)
     hsvcolor = vector(color.rgb_to_hsv(valence))
     if lit:  #primitve activity model:  make it a light source  #local_light(pos=pos, color=(1.0,0,0)) or at least maximize saturation #XXX is this working?
         local_light(pos=pos, color=color.white)
@@ -95,12 +97,17 @@ def gnode(frame, name, pos, mass=mean_node_radius, valence=base_color, lit=False
     else:
         hsvcolor[2] *= 0.3 #reducing lightness
     valence = color.hsv_to_rgb(hsvcolor)
-    sphere(frame=frame, pos=pos, color=valence, opacity=base_opacity, radius=mass, material=materials.emissive if lit else scene.material, **kwargs) #inner sphere  (Note: try material=chrome)
-    sphere(frame=frame, pos=pos, color=atmos(valence), opacity=0.2*base_opacity, radius=mass+noosphere, material=materials.emissive, **kwargs) #outer sphere, color should be converted to hsv and value amplified assuming ambient color is shade of pure white
+    obj = sphere(frame=frame, pos=pos, color=valence, opacity=base_opacity, radius=mass, material=materials.emissive if lit else scene.material, **kwargs) #inner sphere  (Note: try material=chrome)
+    sphere(frame=frame, pos=pos, color=atmos(valence), opacity=0.2*base_opacity, radius=mass+noosphere, material=materials.glass if lit else materials.emissive, **kwargs) #outer sphere, color should be converted to hsv and value amplified assuming ambient color is shade of pure white
+    return obj
 
+
+def link():
+    pass
 ###################################################################
 
 n = Network()  #change this to use Network rather than Graph.
+d = dict()
 f = frame()    #window frame that will hold all the objects
 
 ##Depth-cueing experiment #2: simply place semi-tranparent, scene.ambient-colored boxes from front to back.  They'll get progressively darker away from the user.
@@ -116,8 +123,10 @@ for i in range(num_nodes):
     
     if light:
         print "lit", name
-        
-    gnode(f, name, rand3tuple(), random.randint(1,mean_node_radius), base_color, light, random.randint(int(mean_node_radius/2), mean_node_radius*2))
+
+    radius = random.randint(1,mean_node_radius)
+    d[name]= gnode (f, name, rand3tuple(), radius, base_color, light, mean_node_radius) # random.randint(radius, radius+1))
+
 
 scene.autoscale=False
 
@@ -143,10 +152,10 @@ while True:
     #print(scene.forward)
     if scene.mouse.events:
         mm=scene.mouse.getevent()
-        scene.mouse.getclick()  #assume it's a click, remove from "buffer"
+        scene.mouse.getclick()  #XXX this seems redundant]  assume it's a click, remove from "buffer" (
 
         if mm.pick==None: #create new sphere XXX just testing
-            gnode(f, "light", pos=mm.pos+(0,0,random.randint(-world_radius, +world_radius)), lit=True) #z axis not determinable with 2-d mouse position
+            d["light"] = gnode(f, "light", pos=mm.pos+(0,0,random.randint(-world_radius, +world_radius)), lit=True) #z axis not determinable with 2-d mouse position
         else:
             print mm.pick, mm.pick.radius
             if mm.shift:  #XXX this is crude need to send this to node object so it both spheres get adjusted
@@ -159,4 +168,4 @@ while True:
             break
         #elif key == "Left-Shift"
 
-print(n)
+print(d)
